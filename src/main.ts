9708d64d5d3a4d036329221d7180abef16788e4b
Feliziaboustedt <felizia.boustedt@hotmail.com>
2024-12-10 14:10:37 +0100

    anrop createhtml i button

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ interface IMovie{
 
     const response = await axios.get<IOMDBResponse>("https://img.omdbapi.com/?=c5e0d36b&s=harry");
     
-    console.log(response.data.Search);
+    createHtml(response.data.Search);
   });
 
   function createHtml(movies: IMovie[]) {
