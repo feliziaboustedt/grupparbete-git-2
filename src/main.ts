1a22c2da2975f60c1631e0d9e02435f932b5e4f3
Malin <maa.lin@outlook.com>
2024-12-10 11:48:15 +0100

    added input and btn

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,9 @@ interface IMovie{
     Poster: string;
     imdbID: string;
 }
+
+  document.getElementById("form")?.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    console.log("hej hej");
+  })
